8d9a7be26364c8bcb811e019b20c6e51b1a1c304
“carmen” <hwgguang@163.com>
2016-08-16 15:32:17 +0800

    初始化项目

---
+++ SpotNews/SpotNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotNews
+//
+//  Created by WenGuangHuang on 16/8/16.
+//  Copyright © 2016年 jin10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
